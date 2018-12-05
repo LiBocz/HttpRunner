33f44fda7e75d20a79b87c5c63377da280b445b6
LiBocz <234668430@qq.com>
2018-12-05 10:46:15 +0800

    Update parser.py

--- httprunner/parser.py
+++ httprunner/parser.py
@@ -9,7 +9,7 @@ from httprunner.compat import basestring, builtin_str, numeric_types, str
 
 variable_regexp = r"\$([\w_]+)"
 function_regexp = r"\$\{([\w_]+\([:\$\w\.\-/_ =,]*\))\}"
-function_regexp_compile = re.compile(r"^([\w_]+)\(([\$\w\.\-/_ =,]*)\)$")
+function_regexp_compile = re.compile(r"^([\w_]+)\(([:\$\w\.\-/_ =,]*)\)$")
 
 
 def parse_string_value(str_value):
